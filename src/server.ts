7150828b7c7c4f5add13f0722b1d2039b27a554f
famesensor <sathit.ju@outlook.com>
2021-06-12 00:18:37 +0700

    [:hammer:]: add auth route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 
+import { auth } from './routes/index';
+
 const app: express.Express = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/api/auth', auth);
 
 const port: number = 3000 || process.env.PORT;
 
