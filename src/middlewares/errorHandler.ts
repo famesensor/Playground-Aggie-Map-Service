affdd210c462c034fe8bea924ee04840455545b7
famesensor <sathit.ju@outlook.com>
2021-06-12 01:08:06 +0700

    [:hammer:]: update error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,13 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { errorReponseExtended } from '../models/error';
 
-export const reponseHanler = (
+export const errorHandler = (
     err: errorReponseExtended,
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    console.log(err);
+    let error = { ...err };
 
-    res.status(err.statusCode);
+    error.message = err.message;
+
+    if (error.validateError) {
+        error.errors = error.validateError;
+    }
+
+    res.status(err.statusCode || 500).json({
+        message: error.message,
+        errors: error.errors
+    });
 };
