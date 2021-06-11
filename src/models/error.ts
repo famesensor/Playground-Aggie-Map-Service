5c0760f7da19157c272d3e4ad6fcf60a1d611756
famesensor <sathit.ju@outlook.com>
2021-06-12 01:08:33 +0700

    [:hammer:]: new error validate

--- src/models/error.ts
+++ src/models/error.ts
@@ -4,3 +4,8 @@ export interface errorReponseExtended extends ErrorReponse {
     message: string | any;
     errors: any;
 }
+
+export interface errorValidate {
+    isValid: boolean;
+    errors: object;
+}
