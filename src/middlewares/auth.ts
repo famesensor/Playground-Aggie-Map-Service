4c024b1edbcc9219dd2ce746f97b059cdd066645
famesensor <sathit.ju@outlook.com>
2021-06-12 23:47:14 +0700

    [:sparkles:]: new protect auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { jwtClaims } from '../models/user';
+import { ErrorReponse } from '../utils/error/errorReponse';
+
+export const protect = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer ')
+    ) {
+        let token = req.headers.authorization.split('Bearer ')[1];
+
+        try {
+            let verify = (await jwt.verify(
+                token,
+                process.env.SECRET_KEY!
+            )) as jwtClaims;
+
+            req.user = {
+                user_id: verify.user_id
+            };
+
+            next();
+        } catch (error) {
+            next(new ErrorReponse('invalid token', 401));
+        }
+    } else {
+        next(new ErrorReponse('invalid token', 401));
+    }
+};
