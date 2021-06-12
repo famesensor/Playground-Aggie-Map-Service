f8371c7bc7605efe0603014b90555c37e548a40b
famesensor <sathit.ju@outlook.com>
2021-06-12 23:47:37 +0700

    [:sparkles:]: new route signin , healthz route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
-import { signUp } from '../controllers/auth';
+import { signIn, signUp } from '../controllers/auth';
+import { protect } from '../middlewares/auth';
 
 const router = Router();
 
 router.route('/signup').post(signUp);
+router.route('/signin').post(signIn);
+
+router.route('/healthz').get(protect, (req, res, next) => {
+    res.status(200).json({ message: 'success' });
+});
 
 export default router;
