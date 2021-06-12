4beb68b8cc24fca79795596ffcdfb514cd5ea208
famesensor <sathit.ju@outlook.com>
2021-06-12 22:44:43 +0700

    [:sparkles:]: new interface user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,26 @@
+import { admin } from '../configs/firebase.config';
 export interface signUpCredentials {
     name: string;
     email: string;
     password: string;
+}
+
+export interface signInCredentials {
+    email: string;
+    password: string;
+}
+
+export interface userInterface {
+    user_id: string;
+    name: string;
+    email: string;
+    password: string;
+    createDate: admin.firestore.FieldValue;
+    updateDate: admin.firestore.FieldValue;
+}
+
+export interface jwtClaims {
+    user_id: string;
+    email: string;
+    name: string;
 }
