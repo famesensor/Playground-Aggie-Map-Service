64464f40eb6e6f8b0051020a53affa6527a4558d
famesensor <sathit.ju@outlook.com>
2021-06-16 01:18:08 +0700

    [:sparkles:]: new route map

--- src/routes/maps.ts
+++ src/routes/maps.ts
@@ -1,3 +1,20 @@
 import { Router } from 'express';
 
+import {
+    GetSoils,
+    GetStone,
+    GetWaters,
+    InsertWaters
+} from '../controllers/maps';
+import { protect } from '../middlewares/auth';
 const rotuer = Router();
+
+rotuer.route('/soils').get(protect, GetSoils);
+
+rotuer.route('/stones').get(protect, GetStone);
+
+rotuer.route('/waters').get(protect, GetWaters);
+
+rotuer.route('/insert-water').post(protect, InsertWaters);
+
+export default rotuer;
