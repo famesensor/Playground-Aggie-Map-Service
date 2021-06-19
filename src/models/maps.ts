ad284305108b21456e3b7b7e831899cb26ac8f8d
famesensor <sathit.ju@outlook.com>
2021-06-20 02:22:55 +0700

    [:hammer:]: update interface map

--- src/models/maps.ts
+++ src/models/maps.ts
@@ -1,11 +1,27 @@
 export interface mapInterface {
-    mapId?: string;
+    Id?: string;
     unit: string;
     polyName: string;
     locations: location[];
+    x_max?: number;
+    x_min?: number;
+    y_man?: number;
+    y_min?: number;
 }
 
 export interface location {
     lat: number;
     lng: number;
+}
+
+export interface plan {
+    plan_id?: string;
+    plan_name: string;
+    address: string;
+    location: location;
+    unit?: string;
+    create_by?: string;
+    update_by: string;
+    create_date: Date;
+    update_date: Date;
 }
