ef64e4c6cffccef846bf667d2eb9bee73a4e1c87
famesensor <sathit.ju@outlook.com>
2021-06-20 16:34:29 +0700

    [:sparkles:]: new get plan list

--- src/controllers/maps.ts
+++ src/controllers/maps.ts
@@ -12,28 +12,29 @@ import { asyncHandler } from '../middlewares/asyncHandler';
 import { uuidGen } from '../utils/uidgen/uidgen';
 
 // validate...
-import {} from '../utils/validation/index';
+import { createPlanValidate } from '../utils/validation/index';
 
 // interface...
-import { mapInterface } from '../models/maps';
+import { mapInterface, plan } from '../models/maps';
 
 // error reponse
 import { ErrorReponse } from '../utils/error/errorReponse';
 
-const waterDatabase = db.collection('water');
+const gwavDatabase = db.collection('gwav');
 const soilDatabase = db.collection('soil');
-const stoneDatabase = db.collection('stone');
+const hidoDatabase = db.collection('hido');
+const planDatabase = db.collection('plan');
 
 // get waters...
 export const GetWaters = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-        const waters = await waterDatabase.get();
+        const waters = await gwavDatabase.get();
 
         let watersList: mapInterface[] = [];
         for (let ele of waters.docs) {
             const data = ele.data();
             const temp: mapInterface = {
-                mapId: data.mapId,
+                Id: data.mapId,
                 unit: data.unit,
                 polyName: data.polyName,
                 locations: data.locations
@@ -59,7 +60,7 @@ export const GetSoils = asyncHandler(
         for (let ele of soils.docs) {
             const data = ele.data();
             const temp: mapInterface = {
-                mapId: data.mapId,
+                Id: data.mapId,
                 unit: data.unit,
                 polyName: data.polyName,
                 locations: data.locations
@@ -78,13 +79,13 @@ export const GetSoils = asyncHandler(
 // get stone...
 export const GetStone = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-        const stones = await stoneDatabase.get();
+        const stones = await hidoDatabase.get();
 
         let stonesList: mapInterface[] = [];
         for (let ele of stones.docs) {
             const data = ele.data();
             const temp: mapInterface = {
-                mapId: data.mapId,
+                Id: data.mapId,
                 unit: data.unit,
                 polyName: data.polyName,
                 locations: data.locations
@@ -101,23 +102,127 @@ export const GetStone = asyncHandler(
     }
 );
 
-// insert waters...
-export const InsertWaters = asyncHandler(
+// create plan...
+export const CraetePlan = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-        const raw = fs.readFileSync('./1.csv', 'utf8');
+        const body: plan = req.body;
 
-        const result = await csv(raw, { headers: false });
-        const waters: mapInterface[] = [];
-
-        for (let ele of result) {
-            const uid = uuidGen('waters');
-            // const temp: mapInterface = {
-            //     mapId: uid,
-            //     unit: ele['1'],
-            //     lo
-            // };
+        const { isValid, errors } = createPlanValidate(body);
+        if (!isValid) {
+            return next(new ErrorReponse('Validate Error', 400, errors));
         }
 
+        // const hidoRef = await hidoDatabase.orderBy('x_max', 'desc').get();
+        // for (let ele of hidoRef.docs) {
+        //     const data = ele.data();
+        //     if (
+        //         body.location.lat <= data.x_max &&
+        //         body.location.lat >= data.x_min
+        //     ) {
+        //         if (
+        //             body.location.lng <= data.y_man &&
+        //             body.location.lng >= data.y_min
+        //         ) {
+        //             console.log(data);
+        //              break;
+        //         } else {
+        //             continue;
+        //         }
+        //     } else {
+        //         continue;
+        //     }
+        // }
+
+        // const gwavRef = await gwavDatabase.orderBy('x_max', 'desc').get();
+        // for (let ele of gwavRef.docs) {
+        //     const data = ele.data();
+        //     if (
+        //         body.location.lat <= data.x_max &&
+        //         body.location.lat >= data.x_min
+        //     ) {
+        //         if (
+        //             body.location.lng <= data.y_man &&
+        //             body.location.lng >= data.y_min
+        //         ) {
+        //             console.log(data);
+        //             break;
+        //         } else {
+        //             continue;
+        //         }
+        //     } else {
+        //         continue;
+        //     }
+        // }
+
+        const soilRef = await soilDatabase.orderBy('x_max', 'desc').get();
+        for (let ele of soilRef.docs) {
+            const data = ele.data();
+            if (
+                body.location.lat <= data.x_max &&
+                body.location.lat >= data.x_min
+            ) {
+                if (
+                    body.location.lng <= data.y_man &&
+                    body.location.lng >= data.y_min
+                ) {
+                    console.log(data);
+                    // break;
+                } else {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+        }
+
+        res.status(201).json({ message: 'success' });
+    }
+);
+
+// gat plan list
+export const GetPlanList = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const userId = req.user?.user_id;
+
+        const planRef = await planDatabase
+            .orderBy('create_date', 'desc')
+            .where('create_by', '==', userId)
+            .get();
+
+        let plans: plan[] = [];
+        if (!planRef.empty) {
+            for (let ele of planRef.docs) {
+                let data = ele.data();
+                const temp: plan = {
+                    plan_id: data.plan_id,
+                    plan_name: data.plan_name,
+                    location: {
+                        lat: data.location.lat,
+                        lng: data.location.lng
+                    },
+                    address: data.address,
+                    create_date: data.create_date
+                };
+
+                plans.push(temp);
+            }
+        }
+
+        res.status(200).json({
+            message: 'success',
+            data: {
+                plan: plans
+            }
+        });
+    }
+);
+
+// check
+export const GetCheck = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const hidoRef = await db.collection('soil').get();
+
+        console.log(hidoRef.size);
         res.status(200).json({ message: 'message' });
     }
 );
