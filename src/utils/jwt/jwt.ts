4292ec3342b82f371ac8008b7fc444c6aee8f3f9
famesensor <sathit.ju@outlook.com>
2021-06-24 22:00:59 +0700

    [:hammer:]: update expires-in

--- src/utils/jwt/jwt.ts
+++ src/utils/jwt/jwt.ts
@@ -2,5 +2,5 @@ import jwt from 'jsonwebtoken';
 import { jwtClaims } from '../../models/user';
 
 export const jwtGenerate = (claim: jwtClaims): string => {
-    return jwt.sign(claim, process.env.SECRET_KEY!, { expiresIn: 3600 });
+    return jwt.sign(claim, process.env.SECRET_KEY!, { expiresIn: 86400 });
 };
